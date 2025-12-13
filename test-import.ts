63d0dd9fa627c8b6e4a348d66a14476d8e815984
Maurice Consultant <mainna62@outlook.com>
2025-12-13 22:50:40 +0300

    chore: remove test-import.ts — clean build

--- test-import.ts
+++
@@ -1,2 +0,0 @@
-﻿import { CourseSearchInput } from "@/app/(dashboard)/(routes)/admin/create-admin/[adminId]/course/[courseId]/search/_components/course-search-input";
-console.log(CourseSearchInput);
