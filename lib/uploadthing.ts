7e81d06ad0cf8eab15955aba54ee8a63f3137716
Maurice Consultant <mainna62@outlook.com>
2026-01-30 15:41:32 +0300

    adjusted lib uploadthing

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -1,7 +1,18 @@
-// lib/uploadthing.ts
-import { generateComponents } from "@uploadthing/react";
-import type { OurFileRouter } from "@/app/api/uploadthing/core";
+import { auth } from "@clerk/nextjs/server"
+import { createUploadthing, type FileRouter } from "uploadthing/next"
 
-// Generate typed components
-export const { UploadButton, UploadDropzone, Uploader } =
-  generateComponents<OurFileRouter>();
+const f = createUploadthing()
+
+const handleAuth = async () => {
+  const { userId } = await auth()
+  if (!userId) throw new Error("Unauthorized")
+  return { userId }
+}
+
+export const ourFileRouter = {
+  imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+    .middleware(handleAuth)
+    .onUploadComplete(() => {}),
+} satisfies FileRouter
+
+export type OurFileRouter = typeof ourFileRouter
